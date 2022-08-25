a2a3c9631f398f9d8818aeaa517e80e27e8ac846
Slav Lomachynkyi <slavchek.3777@gmail.com>
2022-08-25 20:56:16 +0300

    add delete function

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PostItem from './PostItem'
 
-const PostList = ({posts}) => {
+const PostList = ({posts, remove}) => {
     return (
         <div>
             <h1 style={{textAlign:'center'}}>
                 Posts about JS
             </h1>
             {posts.map((post, index) => 
-                <PostItem number={index + 1} post={post} key={post.id}/>
+                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
             )}
         </div>
     );
