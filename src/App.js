4942820697ec983ae4e462e8b8830d2f1433bfc2
Slav Lomachynkyi <slavchek.3777@gmail.com>
2022-08-25 20:14:08 +0300

    full fix start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css'
 
 function App() {
@@ -10,41 +9,14 @@ function App() {
         {id:2, name:'JavaScripts2', info:'Sanya hui sosi'},
         {id:3, name:'JavaScripts3', info:'Sanya hui sosi'},
     ])
-    
-    const[name, setName] = useState()
-    const[info, setInfo] = useState()
-
-    const addNewPost = (event) =>{
-        const newPost ={
-            name,
-            info
-        }
+      
+    const createPost = (newPost) =>{
         setPosts([...posts, newPost])
-        setName('')
-        setInfo('')
     }
-  
+
     return (
         <div className='App'>
-            <div className='BlockPost'>
-            <div>
-                <MyInput 
-                    type='text'
-                    placeholder='Название поста' 
-                    value={name}
-                    onChange={event => setName(event.target.value)}
-                />
-            </div>
-            <div>    
-                <MyInput 
-                    type='text'
-                    placeholder='Описание поста'
-                    value={info}
-                    onChange={event => setInfo(event.target.value)}    
-                />
-            </div>
-            <MyButton onClick={addNewPost}>Create a Post</MyButton>
-        </div>
+            <PostForm create={createPost}/>
             <PostList posts={posts}></PostList>
         </div>
   );
